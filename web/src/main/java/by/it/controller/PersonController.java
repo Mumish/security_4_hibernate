99e945f80d763c9f4c2c188313bb12b2d9dd1d10
Mumish <Mudragel@yandex.ru>
2016-03-16 07:37:17 +0300

    недодал контроллер в прошлой ревизии

--- web/src/main/java/by/it/controller/PersonController.java
+++ web/src/main/java/by/it/controller/PersonController.java
@@ -13,6 +13,11 @@ package by.it.controller;
 
 //import by.it.academy.pojos.Person;
 //import by.it.academy.services.IPersonService;
+import by.it.model.User;
+import by.it.model.UserProfile;
+import by.it.model.enums.UserProfileType;
+import by.it.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +27,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/admin")
 public class PersonController {
 
+    @Autowired
+    private UserService userService;
 //    @Autowired
 //    private IPersonService personService;
+
     @RequestMapping(value = "/persons", method = RequestMethod.GET)
     public String mainPage(ModelMap model) {
 //        fillModel(model);
@@ -62,4 +70,33 @@ public class PersonController {
 //        }
 //        model.put("person", person);
 //    }
+    void createAdminIfNeed() {
+
+        User u = userService.findById(1L);
+
+        if (u == null) {
+
+            u = new User();
+
+            u.setUserName("root");
+
+            u.setPassword("root");
+
+            u.setFirstName("admin_first_name");
+
+            u.setLastName("admin_last_name");
+
+            u.setEmail("admin@email.com");
+
+            UserProfile profAdmin = new UserProfile();
+            profAdmin.setType(UserProfileType.ADMIN.getType());
+            u.getUserProfiles().add(profAdmin);
+            UserProfile profDba = new UserProfile();
+            profDba.setType(UserProfileType.DBA.getType());
+            u.getUserProfiles().add(profDba);
+
+            userService.persist(u);
+        }
+
+    }
 }
